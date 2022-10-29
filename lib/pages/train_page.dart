9b02dc23f923554eaf427a570d7f91587d54b171
Peter <sheraium@gmail.com>
2022-10-29 14:34:26 +0800

    modify train page

--- lib/pages/train_page.dart
+++ lib/pages/train_page.dart
@@ -1,23 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
 class TrainPage extends StatefulWidget {
-
   @override
   State<TrainPage> createState() => _TrainPageState();
 }
 
 class _TrainPageState extends State<TrainPage> {
+  String? from = 'munich';
+  String? to = 'neuschwanstein';
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('訂購火車票'),
+      ),
+      body: Column(
         children: [
-          const Text(
-            '火車票',
-            style: TextStyle(fontSize: 30.0),
+          Row(
+            children: [
+              Text('從：', style: TextStyle(fontSize: 20.0),),
+              DropdownButton(
+                style: const TextStyle(fontSize: 20.0, color: Colors.black),
+                value: from,
+                items: const [
+                  DropdownMenuItem(value: 'munich', child: Text('慕尼黑')),
+                ],
+                onChanged: (String? value) {
+                  if (value == null) return;
+                  setState(() {
+                    from = value;
+                  });
+                },
+              ),
+            ],
           ),
+          Row(
+            children: [
+              Text('到：', style: TextStyle(fontSize: 20.0),),
+              DropdownButton(
+                style: const TextStyle(fontSize: 20.0, color: Colors.black),
+                value: to,
+                items: const [
+                  DropdownMenuItem(value: 'neuschwanstein', child: Text('新天鵝堡')),
+                ],
+                onChanged: (String? value) {
+                  if (value == null) return;
+                  setState(() {
+                    to = value;
+                  });
+                },
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final Uri _url =
+                  Uri.parse('https://buy.stripe.com/test_7sIeXHcn42sEcG48ww');
+              if (!await launchUrl(_url)) {
+                throw 'Could not launch $_url';
+              }
+            },
+            child: const Text(
+              '立即訂票',
+              style: TextStyle(
+                fontSize: 30.0,
+              ),
+            ),
+          )
         ],
       ),
     );
